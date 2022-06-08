7b0cea529bc8094ac18e355978fbb5defa8d6521
RoughGhost <stephendlouis6@gmail.com>
2022-06-09 00:33:50 +0100

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import { Header, Navbar } from "../../components";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
